ae1e45b53488afea8d709701c9a4676843880844
JBS-ryota-kondo <ryota.kondo@seedias.cc>
2023-07-30 01:34:38 +0900

    init

--- lib/schedule/schedule_model.dart
+++ lib/schedule/schedule_model.dart
@@ -1,14 +1,5 @@
-import '/flutter_flow/flutter_flow_animations.dart';
-import '/flutter_flow/flutter_flow_charts.dart';
-import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
-import '/flutter_flow/random_data_util.dart' as random_data;
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
-import 'package:flutter_animate/flutter_animate.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:provider/provider.dart';
 
 class ScheduleModel extends FlutterFlowModel {
   ///  State fields for stateful widgets in this page.
